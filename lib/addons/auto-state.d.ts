3a32ccf0fd9eb348bb1fec5a871cc7addd27f1d6
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-11-29 13:49:44 -0600

    Update import

--- lib/addons/auto-state.d.ts
+++ lib/addons/auto-state.d.ts
@@ -1,7 +1,7 @@
 import Addon from './addon'
-import StateAddon from './state-addon'
+import StateAddon from './state'
 
-declare class AutoStateAddon<T1 extends Addon, T2 extends StateAddon<unknown>> extends Addon {
+declare class AutoStateAddon<T1 extends Addon, T2 extends StateAddon<Addon>> extends Addon {
   constructor(headAddon: T1, ...addons: Array<T2>)
 
   headAddon: T1
